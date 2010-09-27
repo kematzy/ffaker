e32c7eaeeedfb7a19a338cf00176e2d79b18412d
kematzy <kematzy@gmail.com>
2010-09-27 10:18:44 +0800

    Added support for #dialing_code
    * aliased as #international_dialing_code
    * aliased as #country_dialing_code
    * actual ISO dialing codes used

--- test/test_phone_number.rb
+++ test/test_phone_number.rb
@@ -8,4 +8,8 @@ class TestPhoneNumer < Test::Unit::TestCase
   def test_short_phone_number
     assert_match /\d{3}-\d{3}-\d{4}/, Faker::PhoneNumber.short_phone_number
   end
+  
+  def test_international_dialing_code
+    assert_match /(\d{1,3}|\d\s\d{1,3})/, Faker::PhoneNumber.international_dialing_code
+  end
 end
